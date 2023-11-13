43d3a90c59d9c2e54c21b7d250b0bd0dba20a582
Alex King <alex.king@and.digital>
2023-11-13 20:47:54 +0000

    Install ws package

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const fs = require("fs");
-const https = require("https");
-const WebSocket = require("ws");
+const fs = require('fs');
+const https = require('https');
+const WebSocket = require('ws');
 
 /* Config */
 
 const port = process.env.PORT || 8080;
 const wssConfig = {};
 let httpsServer;
-if (process.env.USE_HTTPS_SERVER === "true") {
+if (process.env.USE_HTTPS_SERVER === 'true') {
   httpsServer = https.createServer({
-    cert: fs.readFileSync("server.crt"),
-    key: fs.readFileSync("server.key"),
+    cert: fs.readFileSync('server.crt'),
+    key: fs.readFileSync('server.key')
   });
   wssConfig.server = httpsServer;
 } else {
@@ -26,39 +26,39 @@ let idCounter = 0;
 
 /* WebSockets */
 
-wss.on("connection", (ws) => {
+wss.on('connection', (ws) => {
   // Add to list of connected clients
   ws.id = idCounter++;
   clients.push(ws);
 
-  ws.on("message", (message) => {
+  ws.on('message', (message) => {
     switch (message.type) {
-      case "SET_NAME":
+      case 'SET_NAME':
         // Set name on the socket
         ws.name = message.value;
 
         // Notify other clients
         clients.forEach((client) =>
           client.send({
-            type: "USER_JOIN",
-            value: message.value,
+            type: 'USER_JOIN',
+            value: message.value
           })
         );
         break;
 
-      case "MESSAGE_SEND":
+      case 'MESSAGE_SEND':
         // Send out to all clients
         clients.forEach((client) =>
           client.send({
-            type: "MESSAGE_RECEIVE",
-            value: message.value,
+            type: 'MESSAGE_RECEIVE',
+            value: message.value
           })
         );
         break;
     }
   });
 
-  ws.on("close", () => {
+  ws.on('close', () => {
     // Remove from client list
     clients.splice(
       clients.findIndex((client) => client.id === ws.id),
@@ -69,8 +69,8 @@ wss.on("connection", (ws) => {
     if (ws.name) {
       clients.forEach((client) =>
         client.send({
-          type: "USER_LEAVE",
-          value: ws.name,
+          type: 'USER_LEAVE',
+          value: ws.name
         })
       );
     }
@@ -80,3 +80,5 @@ wss.on("connection", (ws) => {
 if (httpsServer) {
   httpsServer.listen(port);
 }
+
+console.debug(`Server started on port ${port}`);
